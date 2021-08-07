d6bc979d097c036e4d751292eb3962dd981b4a21
edimar-m <edimar.morles@gmail.com>
2021-08-07 13:43:37 -0300

    ADD/Añadiendo contenido funcionalidad popular movies, para hacer pull en gh-pages

---
+++ src/pages/popular-movies.jsx
@@ -0,0 +1,5 @@
+import { useState, useEffect } from "react";
+import { URL_API, API_KEY } from "../utils/constants";
+import MovieDeploy from "../components/MovieDeploy";
+import Pagination from "../components/Pagination";
+import Loading from '../components/Loading';
