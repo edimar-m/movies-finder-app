298693d121ba7889cfa4a27e21788a7fed4e1e47
edimar-m <edimar.morles@gmail.com>
2021-08-07 13:46:16 -0300

    REMOVE/eliminando contenido de popular-movies para hacer pull en gh-pages

--- src/pages/popular-movies.jsx
+++
@@ -1,5 +0,0 @@
-import { useState, useEffect } from "react";
-import { URL_API, API_KEY } from "../utils/constants";
-import MovieDeploy from "../components/MovieDeploy";
-import Pagination from "../components/Pagination";
-import Loading from '../components/Loading';
